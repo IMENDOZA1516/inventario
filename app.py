64394cf5dfe75dc659c09898a0703551ee4dbf8d
IMENDOZA1516 <127427378+IMENDOZA1516@users.noreply.github.com>
2025-04-23 14:17:49 -0600

    Migración: cambios en tablas

--- app.py
+++ app.py
@@ -1311,7 +1311,7 @@ def exportar_excel_eliminadas():
 
 
 
-if __name__ == '__main__':
-    with app.app_context():
-     db.create_all() 
-     app.run(debug=True)
+#if __name__ == '__main__':
+ #   with app.app_context():
+  #   db.create_all() 
+  #   app.run(debug=True)
